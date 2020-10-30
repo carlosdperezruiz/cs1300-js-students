04be5e938ea6c362a84e55f31b1bae02466e91aa
Carlos Perez <carlosdperezruiz@gmail.com>
2020-10-29 20:30:21 -0400

    submit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=EdvkBV3dUNUzQxIpc9g87eE2JZnkHexAZ_aH3RHiSr8";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -25,7 +25,31 @@ corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      const plantData = JSON.parse(request.response).data;
+      build(plantData);
     })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+const build = (plantData) => {
+  console.log(plantData);
+  plantData.sort(function(a, b) {
+    return a.common_name > b.common_name;
+  });
+
+  plantData.forEach(element => {
+    let div = document.createElement('div');
+    div.id = 'child';
+
+    let name = document.createElement('p')
+    name.innerHTML = element.common_name;
+    div.appendChild(name);
+
+    let img = document.createElement('img')
+    img.src = element.image_url;
+    img.width = 400;
+    div.appendChild(img);
+
+    document.getElementById("flex").appendChild(div);
+  });
+}
